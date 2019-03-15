5f1d8564f15863bc255d8131392b1292a3d7576c
xzh1998 <46914173+xzh1998@users.noreply.github.com>
2019-03-15 16:40:02 +0800

    更新
    打包API

--- 221600436&221600437/src/Main.java
+++ 221600436&221600437/src/Main.java
@@ -1,17 +1,26 @@
 public class Main {
-
+    static Result result=new Result();
     public static void main(String args[]){
         long start,end;
         start = System.currentTimeMillis();
 
         String inputFileName="input77.txt",outputFileName="result.txt";//指定输入输出文件名
         int n=10,CodeMode=0;
-        Result tempresult=new Result();
 
         inputFileName=args[0];
-        Process.read_file(inputFileName,outputFileName,tempresult, CodeMode,n);
+        Process.read_file(inputFileName,outputFileName,result, CodeMode,n);
 
         end = System.currentTimeMillis();
         System.out.println("start time:" + start+ "; end time:" + end+ "; Run Time:" + (end - start) + "(ms)");
+    }
+
+    public int getWord_count() {
+        return result.getWord_count();
+    }
+    public int getChar_count() {
+        return result.getChar_count();
+    }
+    public int getLine_count() {
+        return result.getLine_count();
     }
 }
